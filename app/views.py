0025472e8127836305c77000ffe7555915bf90de
harshadvali11 <harshadvali1431@gmail.com>
2023-04-14 16:51:47 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -10,3 +10,34 @@ def insert_topic(request):
         return HttpResponse('Topic insertion is done Successfully')
 
     return render(request,'insert_topic.html')
+
+def insert_webpage(request):
+    LTO=Topic.objects.all()
+    d={'topics':LTO}
+
+    if request.method=='POST':
+        topic=request.POST['topic']
+        name=request.POST.get('name')
+        url=request.POST.get('url')
+        email=request.POST['email']
+        TO=Topic.objects.get(topic_name=topic)
+
+        WO=Webpage.objects.get_or_create(topic_name=TO,name=name,url=url,email=email)[0]
+        WO.save()
+        return HttpResponse('Webpage insertion is done Successfully')
+
+
+    return render(request,'insert_webpage.html',d)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- project26/urls.py
+++ project26/urls.py
@@ -19,6 +19,7 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('insert_topic/',insert_topic,name='insert_topic'),
+    path('insert_webpage/',insert_webpage,name='insert_webpage'),
 ]
 
 
